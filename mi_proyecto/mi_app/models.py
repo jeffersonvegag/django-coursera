3cada1306fc768615b9acd10cf0acbed370c8a85
Jeff <jefferson.vegag@gamil.com>
2025-03-12 21:30:57 -0500

    fisrt commit

--- mi_proyecto/mi_app/models.py
+++ mi_proyecto/mi_app/models.py
@@ -5,3 +5,18 @@ from django.db import models
 class Categoria(models.Model):
     nombre=models.CharField(max_length=100)
     descripcion= models.CharField()
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+    
