55d33e301f7ee88944fe1b9dba2a2c18f31e6a9e
jeffersonvegag <jefferson.vegag@gmail.com>
2025-03-14 20:17:43 -0500

    add field stock

--- mi_proyecto/api/serializers.py
+++ mi_proyecto/api/serializers.py
@@ -4,7 +4,7 @@ from .models import Categoria, Producto
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
-        fields = ['id', 'nombre', 'descripcion', 'precio', 'categoria', 'fecha_creacion', 'disponible']
+        fields = ['id', 'nombre', 'descripcion', 'precio', 'categoria', 'fecha_creacion', 'disponible','stock']
 
 class CategoriaSerializer(serializers.ModelSerializer):
     productos = ProductoSerializer(many=True, read_only=True)
